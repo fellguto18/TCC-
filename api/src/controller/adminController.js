e3d1f933fffcd376480e0e79b4bac2bab01ab681
Fellipe <fellguto@gmail.com>
2022-09-13 16:07:44 -0300

    aaa

---
+++ api/src/controller/adminController.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { login } from '../repository/UsuarioRepository.js';
+const server = Router();
+
+server.post('/usuario/login', async (req, resp) => {
+    try{
+        const {email, senha} = req.body;
+        const resposta =  await login(email, senha);
+        if(!resposta){
+            throw new Error('Credenciais inválidas')
+        }
+
+
+        resp.send(resposta)
+    }catch(err){
+        resp.status(401).send({
+            erro: err.message
+        })
+    }
+})
+
+export default server;
