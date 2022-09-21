db1e601e6b39980bb5ecc1ecb6e4a59845b5c0c5
Lara Assunção Vieira <assuncaolara96@email.com>
2022-09-21 15:12:39 -0300

    tentando fazer pagina de cadastro do cliente

--- site/src/index.js
+++ site/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import MenuAdmin from './components/menuAdmin';
 import CadastroAdm from './pages/CadastartAdm';
+import CriarConta from './pages/CriarConta';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CadastroAdm />
+    <CriarConta />
   </React.StrictMode>
 );
 
---
+++ site/src/pages/CriarConta/index.jsx
@@ -0,0 +1,10 @@
+import './index.scss'
+import logo from '../../assets/images/logo.png'
+import ftcriarconta from '../../assets/images/ftcriarconta.png'
+
+
+function CriarConta(){
+    return(
+   
+    )}
+export default CriarConta;
