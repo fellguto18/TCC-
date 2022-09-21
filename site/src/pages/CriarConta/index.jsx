681e1caa8edc2b304aa65204b0a1cc748f583b7c
Lara Assunção Vieira <assuncaolara96@email.com>
2022-09-21 15:21:06 -0300

    a

--- site/src/pages/CriarConta/index.jsx
+++ site/src/pages/CriarConta/index.jsx
@@ -5,6 +5,11 @@ import ftcriarconta from '../../assets/images/ftcriarconta.png'
 
 function CriarConta(){
     return(
-   
+        <div className='page'>
+            <div className='informacoes'>
+                <p></p>
+            </div>
+            <div className='inf-img'></div>
+        </div>
     )}
 export default CriarConta;
