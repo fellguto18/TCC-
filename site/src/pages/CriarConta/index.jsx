7c1848c6f5afd0b801dbbff4a5b5175293596b07
Lara Assunção Vieira <assuncaolara96@gmail.com>
2022-10-04 15:20:26 -0300

    aaa

--- site/src/pages/CriarConta/index.jsx
+++ site/src/pages/CriarConta/index.jsx
@@ -7,13 +7,10 @@ import a from '../../assets/images/a.png'
 export default function CriarConta(){
     return(
         <div className='page'>
-             <img className='logo' src={logo} />
-             <div className='aaaa'>
+<div>             <img className='logo' src={logo} /></div>
+            <div className='aaaa'>
                 <div className='informacoes'>
-                    
-                    <div className='voltar'>
-                        <img className='a' src={a} /><h1>Voltar </h1>
-                    </div>
+                    <h1>Voltar</h1>
                     <p>Nome Completo</p>
                     <input type="text" />
                     <p>CPF</p>
