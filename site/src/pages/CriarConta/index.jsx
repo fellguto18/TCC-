5851b9938726f8f09596158ca6e80fe3d7159af1
Lara Assunção Vieira <assuncaolara96@gmail.com>
2022-10-04 16:26:46 -0300

    hugo

--- site/src/pages/CriarConta/index.jsx
+++ site/src/pages/CriarConta/index.jsx
@@ -1,32 +1,34 @@
 import './index.scss'
 import logo from '../../assets/images/logo.png'
 import ftcriarconta from '../../assets/images/ftcriarconta.png'
-import a from '../../assets/images/a.png'
+
 
 
 export default function CriarConta(){
     return(
-        <div className='page'>
-<div>             <img className='logo' src={logo} /></div>
-            <div className='aaaa'>
-                <div className='informacoes'>
-                    <h1>Voltar</h1>
-                    <p>Nome Completo</p>
-                    <input type="text" />
-                    <p>CPF</p>
-                    <input type="text" />
-                    <p>E-mail</p>
-                    <input type="text" />
-                    <p>Senha</p>
-                    <input type="text" />
-                    <p>Confirme sua senha</p>
-                    <input type="text" />
-                    <button className='botaoCadastar'>Cadastrar</button>
+        <div className=''>
+                <div className='logo'> <img src={logo} /></div>
+            <div className='b'>
+                <div className='aaaa'>
+                    <div className='informacoes'>
+                        <button className='utton'>Voltar</button>
+                        <p>Nome Completo</p>
+                        <input type="text" />
+                        <p>CPF</p>
+                        <input type="text" />
+                        <p>E-mail</p>
+                        <input type="text" />
+                        <p>Senha</p>
+                        <input type="text" />
+                        <p>Confirme sua senha</p>
+                        <input type="text" />
+                        <button className='tton'>Cadastrar</button>
+                    </div>
+                    <div className='inf-img'>
+                            <img src={ftcriarconta} />
+                    </div>
                 </div>
-                <div className='inf-img'>
-                        <img src={ftcriarconta} />
-                </div>
-             </div>
+            </div>
         </div>
 )
 }
