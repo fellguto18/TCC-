dc228a7a1a44893a20026f903bde6b8d0e7ec674
gabrieukk <gabriel0764@hotmail.com>
2022-10-24 23:45:50 -0300

    CONTINUAÇÃO LANDING PAGE

--- site/src/components/header/index.jsx
+++ site/src/components/header/index.jsx
@@ -1,4 +1,6 @@
 import './index.scss'
+import '../../common.scss'
+
 import { Link } from "react-router-dom";
 
 export default function NavBarComp(){
--- site/src/pages/LandingPage/index.jsx
+++ site/src/pages/LandingPage/index.jsx
@@ -10,10 +10,25 @@ export default function LandingPage(){
         <>
         <main>
             <NavBarComp/>
-            <section>
-                <picture> <img src='/assets/images/imgonelp.svg' alt="Pessoas reunidas" /></picture>
-                <div><p>Nós somos uma organização que ajuda pessoas, sem fins lucrativos, este projeto foi feito através de trabalhos voluntários.</p></div>
+
+            <section className='landing-page-f1'>
+                <picture><img src="/assets/images/imgonelp.svg" alt="" /></picture>
+                <div className='landing-page-text'><p>Nós somos uma organização que tem apenas a intenção de ajudar pessoas. Sem fins lucrativos, este projeto foi feito através de trabalhos voluntários.</p></div>
             </section>
+
+            <section className='landing-page-f2'>
+                <picture> <img src="/assets/images/manlp.svg" alt="Pessoa com um coração roxo" /></picture>
+                <div className='landing-page-text'><p>Os projetos são diretamente para pessoas necessitadas, para ajudá-las.</p></div>
+            </section>
+
+            <section className='landing-page-f3'>
+                <h2>Projetos ativos</h2>
+                <div>
+                    Carrosel em construção
+                </div>
+            </section>
+
+            <Footer/>
             
         </main>
         </>
