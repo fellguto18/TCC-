9c4d55887aeb825d4d7c640ebc1b2eb428131202
Lara Assunção Vieira <assuncaolara96@gmail.com>
2022-10-26 22:10:49 -0300

    aaaaaa

---
+++ site/src/components/detalhe/index.js
@@ -0,0 +1,19 @@
+import 'index.scss';
+
+export default function Detalhes(){
+    return(
+        <div>
+            <div>
+            <aside>
+            <div className='side-menu-links'>
+                <p>Projetos</p>
+                <p>Doadores</p>
+                <p>Estatísticas</p>
+                <p>Sair</p>
+            </div> 
+      </aside>
+      
+            </div>
+        </div>
+    )
+}
--- site/src/routes/index.js
+++ site/src/routes/index.js
@@ -13,6 +13,7 @@ import SobreNos from '../pages/SobreNos';
 import DoacaoRealizada from '../pages/ConfirmacaoDoacao';
 import Grafico from '../pages/Grafico';
 import SenhaAltSucesso from '../pages/SenhaSucesso';
+import MenuAdmin from '../components/menuAdmin'
 
 export default function Rotas(){
 return(
@@ -20,7 +21,7 @@ return(
         <Routes>
             <Route path='/' element = {<LandingPage />} />
             <Route path="/admin/login"  element={<CadastroAdm/>}/>
-            <Route path="/admin/menu"  element={<CadastrarProjeto/>} />
+            <Route path="/admin/cadastrar"  element={<CadastrarProjeto/>} />
             <Route path="/admin/doadores" element={<PesquisarDoadores/>}/>
             <Route path="/usuario/cadastrar"  element={<CriarConta/>} />
             <Route path='/cadastro/agradecimento' element={<CadastroOk/>}/>
@@ -28,7 +29,7 @@ return(
             <Route path='/esqueceu/senha' element={<EsqueceuSenha/>} />
             <Route path='/nova/senha' element={<NovaSenha/>}/>
             <Route path='/sobreNos' element={<SobreNos/>}/>
-
+            <Route path='/admin/menu' element={<MenuAdmin/>}/>
             <Route path='/teste'  element={<AAA/>}/>
             <Route path='/doacao/realizada' element={<DoacaoRealizada/>}/>
             <Route path='/grafico' element={<Grafico/>}/>
