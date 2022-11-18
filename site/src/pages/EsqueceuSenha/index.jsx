a00261f6f6b60ead2c560d53035326fb80c32328
Hugo de Jesus <hugojesussousa10@gmail.com>
2022-11-18 18:11:05 -0300

    esqueci senha 'obs, alterar o botao de enviar'

--- site/src/pages/EsqueceuSenha/index.jsx
+++ site/src/pages/EsqueceuSenha/index.jsx
@@ -5,38 +5,38 @@ import logo from '../../assets/images/logo.png'
 
 
 import React from 'react';
-import { Link ,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import storage from 'local-storage';
 import { useRef } from 'react';
 import { enviarEmail } from "../../api/usuarioApi";
 
 
 
-export default function EsqueceuSenha(){
+export default function EsqueceuSenha() {
 
     const [email, setEmail] = useState('');
-    
-    async function emailUser(){
+
+    async function emailUser() {
         try {
-            
+
             const emailUsuario = await enviarEmail(email);
             setEmail(emailUsuario)
             toast('E-mail enviado.')
         } catch (err) {
             toast('Erro')
         }
-    }   
+    }
 
 
 
 
     const navigate = useNavigate();
-     const ref = useRef();
-    function voltarClick(){
+    const ref = useRef();
+    function voltarClick() {
         storage.remove('usuario-logado');
         navigate('/usuario/cadastrar');
     }
-    return(
+    return (
         <div className='page-esqueceusenha'>
             <div className='logo-1'>
                 <img src={logo} />
@@ -45,17 +45,12 @@ export default function EsqueceuSenha(){
                 <h1>Esqueci a senha</h1>
                 <p>Para redefinir sua senha, nos informe seu e-mail já cadastrado no site.</p>
 
-                <h4>Email</h4>
-                <input className='texto' type="text" placeholder='email@email.com' value={email} onChange={e => setEmail(e.target.value)} />
-
-
-
-
-
+                <h4 className="emei">Email</h4>
+                <input className='botau' type="text" placeholder='Insira seu E-mail aqui' value={email} onChange={e => setEmail(e.target.value)} />
 
                 <div className='div-botao'>
-                    <button onClick={emailUser}>Enviar</button>
-                    <Link to=''className='botao-1' onClick={voltarClick}>Voltar</Link>
+                    <button onClick={emailUser} className='botoun'>Enviar</button>
+                    <Link to='' className='botao-1' onClick={voltarClick}>Voltar</Link>
                 </div>
             </div>
         </div>
